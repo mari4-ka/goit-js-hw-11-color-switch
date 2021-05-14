573790b7e6d57db3ca097a62347f0da0872ffacd
Maria Belik <mashabelik@gmail.com>
2021-05-14 13:57:53 +0300

    add color switch

---
+++ index.js
@@ -0,0 +1,39 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const DELAY = 1000;
+let timerId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startButton = document.querySelector('[data-action="start"]');
+const stopButton = document.querySelector('[data-action="stop"]');
+
+startButton.addEventListener('click', onStartButtonClick);
+
+function onStartButtonClick(e) {
+    console.log('CLICK!!!');
+    
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+        console.log('color change')
+    }, DELAY);
+  startButton.removeEventListener('click', onStartButtonClick);
+  stopButton.addEventListener('click', onStopButtonClick);
+}
+
+function onStopButtonClick() {
+  console.log('stop click');
+
+  clearInterval(timerId);
+
+  stopButton.removeEventListener('click', onStopButtonClick);
+}
